658ba7e124633e80357691994360b715c6954667
psun631773 <psun631773@gmail.com>
2024-03-31 15:58:15 -0400

    isselected update

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,24 +1,35 @@
-import React from 'react'
+import React from "react";
+import useConversation from "../../zustand/useConversation";
+const Conversation = ({ conversation, lastIdx, emoji }) => {
+  const { selectedConversation, setSelectedConversation } =
+    useConversation();
+    // 被选中的底色是蓝色
+  const isSelected = selectedConversation?._id === conversation._id;
+  return (
+    <>
+      <div
+        className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer
+				${isSelected ? "bg-sky-500" : ""}
+			`}
+        onClick={() => setSelectedConversation(conversation)}
+      >
+        <div className="avatar online">
+          <div className="w-12 rounded-full">
+            <img src={conversation.profilePic} alt="user avatar" />
+          </div>
+        </div>
+        <div className="flex flex-col flex-1">
+          <div className="flex gap-3 justify-between">
+            <p className="font-bold text-gray-200">
+              {conversation.fullName}
+            </p>
+            <span className="text-xl">{emoji}</span>
+          </div>
+        </div>
+      </div>
+      {!lastIdx && <div className="divider my-0 py-0 h-1" />}
+    </>
+  );
+};
 
-const Conversation = () => {
-    return (
-        <>
-            <div className="flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 coursor-pointer">
-                <div className="avatar online">
-                    <div className='w-12 rounded-full'>
-                        <img src="" alt="user avatar" />
-                    </div>
-                </div>
-                <div className='flex flex-col flex-1'>
-                    <div className='flex gap-3 justify-between'>
-                        <p className='font-bold text-gray-200'>John Doe</p>
-                        <span className='text-xl'>😀</span>
-                    </div>
-                </div>
-            </div>
-            <div className='divider my-0 py-0 h-1' />
-        </>
-    )
-}
-
-export default Conversation
+export default Conversation;
--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
-import Conversation from './Conversation'
-
+import React from "react";
+import Conversation from "./Conversation";
+import useGetConversations from "../../hooks/useGetConversations";
+import { getRandomEmoji } from "../../util/emojis";
 const Conversations = () => {
+  const { loading, conversations } = useGetConversations();
+  console.log(
+    "%cfrontendsrccomponentssidebarConversations.jsx:6 object",
+    "color: #007acc;",
+    conversations
+  );
   return (
-    <div className='py-2 flex flex-col overflow-auto'>
-      <Conversation/>
-      <Conversation/>
-      <Conversation/>
-      <Conversation/>
-      <Conversation/>
-    </div>
-  )
-}
+    <div className="py-2 flex flex-col overflow-auto">
+      {conversations.map((conversation, idx) => (
+        <Conversation
+          key={conversations._id}
+          conversation={conversation}
+          emoji={getRandomEmoji()}
+          lastIdx={idx === conversations.length - 1}
+        />
+      ))}
 
-export default Conversations
+      {loading ? (
+        <span className="loading loading-spinner mx-auto"></span>
+      ) : null}
+    </div>
+  );
+};
+
+export default Conversations;
